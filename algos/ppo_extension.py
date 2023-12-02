18c0a2f93c33ec4ed5ef1733dab934b422498143
Ben <bencer@outlook.com>
2023-12-02 10:50:01 +0200

    Training: base PPO with decreasing variance and entropy

---
+++ algos/ppo_extension.py
@@ -0,0 +1,71 @@
+from .agent_base import BaseAgent
+from .ppo_utils import Policy
+from .ppo_agent import PPOAgent
+
+import utils.common_utils as cu
+import torch
+import numpy as np
+import torch.nn.functional as F
+import time
+
+class PPOExtension(PPOAgent):
+    
+    def ppo_update(self, states, actions, rewards, next_states, dones, old_log_probs, targets):
+        action_dists, values = self.policy(states)
+        values = values.squeeze()
+        new_action_probs = action_dists.log_prob(actions)
+        ratio = torch.exp(new_action_probs - old_log_probs)
+        clipped_ratio = torch.clamp(ratio, 1-self.clip, 1+self.clip)
+
+        advantages = targets - values
+        advantages -= advantages.mean()
+        advantages /= advantages.std()+1e-8
+        advantages = advantages.detach()
+        policy_objective = -torch.min(ratio*advantages, clipped_ratio*advantages)
+
+        value_loss = F.smooth_l1_loss(values, targets, reduction="mean")
+
+        policy_objective = policy_objective.mean()
+        entropy = action_dists.entropy().mean()
+        loss = policy_objective + 0.5*value_loss - 0.01*entropy
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+    
+    def train_iteration(self,ratio_of_episodes):
+        # Run actual training        
+        reward_sum, episode_length, num_updates = 0, 0, 0
+        done = False
+
+        # Reset the environment and observe the initial state
+        observation, _  = self.env.reset()
+
+        while not done and episode_length < self.cfg.max_episode_steps:
+            # Get action from the agent
+            action, action_log_prob = self.get_action(observation)
+            previous_observation = observation.copy()
+
+            # Perform the action on the environment, get new state and reward
+            observation, reward, done, _, _ = self.env.step(action)
+            
+            # Store action's outcome (so that the agent can improve its policy)
+            self.store_outcome(previous_observation, action, observation,
+                                reward, action_log_prob, done)
+
+            # Store total episode reward
+            reward_sum += reward
+            episode_length += 1
+
+            # Update the policy, if we have enough data
+            if len(self.states) > self.cfg.min_update_samples:
+                self.update_policy()
+                num_updates += 1
+
+                # Update policy randomness
+                self.policy.set_logstd_ratio(ratio_of_episodes)
+
+        # Return stats of training
+        update_info = {'episode_length': episode_length,
+                    'ep_reward': reward_sum}
+        return update_info
---
+++ algos/ppo_utils.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn.functional as F
+from torch.distributions import MultivariateNormal
+import numpy as np
+
+class Policy(torch.nn.Module):
+    def __init__(self, state_space, action_space, env, hidden_size=32):
+        super().__init__()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.env = env
+        self.actor_logstd = 0
+       
+        self.fc1_a = torch.nn.Linear(state_space, hidden_size)
+        self.fc2_a = torch.nn.Linear(hidden_size, hidden_size)
+        self.fc3_a = torch.nn.Linear(hidden_size, action_space)
+
+        self.fc1_c = torch.nn.Linear(state_space, hidden_size)
+        self.fc2_c = torch.nn.Linear(hidden_size, hidden_size)
+        self.fc3_c = torch.nn.Linear(hidden_size, 1)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.normal_(m.weight, 0, 1e-1)
+                torch.nn.init.zeros_(m.bias)
+                
+    def set_logstd_ratio(self, ratio):
+        self.actor_logstd = ratio
+
+    def forward(self, x):
+        x_a = self.fc1_a(x)
+        x_a = F.relu(x_a)
+        x_a = self.fc2_a(x_a)
+        x_a = F.relu(x_a)
+        x_a = self.fc3_a(x_a)
+
+        x_c = self.fc1_c(x)
+        x_c = F.relu(x_c)
+        x_c = self.fc2_c(x_c)
+        x_c = F.relu(x_c)
+        x_c = self.fc3_c(x_c)
+
+        action_mean = torch.tanh(x_a)
+        action_dist = MultivariateNormal(action_mean, scale_tril=torch.diag(0.2 * np.exp(self.actor_logstd) * torch.ones(self.action_space)))
+
+        return action_dist, x_c
